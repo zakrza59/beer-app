4923fcd051a4625903492cc30c6e4c84ce14e28c
zakrza59 <zakrza59@gmail.com>
2018-05-26 13:58:22 +0200

    Added pagination

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
-import Loading from '../loading/Loading.js';
-import Table from './Table.js'
+import Loading from '../loading/Loading';
+import Table from './Table.js';
+import Pagination from '../pagination/Pagination'
 
 
 class List extends React.Component {
@@ -12,13 +13,23 @@ class List extends React.Component {
     this.state = {
       loading: false,
       beers: [],
+      page: 1,
+      totalPages: 12,
     };
+
+    this.handlePaginationClick = this.handlePaginationClick.bind(this);
   }
 
   componentDidMount() {
+    this.fetcher();
+  }
+
+  fetcher() {
     this.setState({ loading: true });
 
-    fetch(`${API_URL}?page=1&per_page=20`)
+    const { page } = this.state;
+    
+    fetch(`${API_URL}?page=${page}&per_page=20`)
     .then(handleResponse)
 
     .then((data) => {
@@ -28,18 +39,34 @@ class List extends React.Component {
         loading: false 
       });
     });
-    
+  }
+
+  handlePaginationClick(direction) {
+    let nextPage = this.state.page;
+
+    nextPage = direction === 'next' ? nextPage + 1 : nextPage - 1;
+
+    this.setState({ page: nextPage }, () => {
+      this.fetcher();
+    })
   }
 
   render() {
 
-    const { loading, beers } = this.state;
+    const { loading, beers, page, totalPages } = this.state;
 
     if (loading) {
       return <div className="Loading-container"><Loading /></div>
     }
     return (
-      <Table beers={beers} />
+      <div>
+        <Table beers={beers} />
+        <Pagination 
+        page={page}
+        totalPages={totalPages}
+        handlePaginationClick={this.handlePaginationClick}
+        />
+      </div>
     );
   }
 }
